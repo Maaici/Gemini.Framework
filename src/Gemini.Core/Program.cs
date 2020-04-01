6daacc1a38a091de6c5f073bb9e37806be68362b
Maaici <724556872@qq.com>
2020-04-01 14:01:38 +0800

    添加第三方日志组件（serilog）

--- src/Gemini.Core/Program.cs
+++ src/Gemini.Core/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System.IO;
 
 namespace Gemini.Core
 {
@@ -7,11 +9,25 @@ namespace Gemini.Core
     {
         public static void Main(string[] args)
         {
+            //第三方log工具serilog的相关配置
+            Log.Logger = new LoggerConfiguration()
+                //配置日志最小输出的级别为：debug
+                .MinimumLevel.Warning()
+                //如果是Microsoft的日志，最小记录等级为info
+                //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                //输出到控制台
+                .WriteTo.Console()
+                //将日志保存到文件中（两个参数分别是日志的路径和生成日志文件的频次，当前是一天一个文件）
+                .WriteTo.File(Path.Combine("logs", @"log.txt"), rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .ConfigureLogging(config => config.AddSerilog())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
